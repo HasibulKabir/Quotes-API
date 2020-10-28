205a222686725f7094db0aab882cae159c8ad5f3
Hasibul Kobir <46620128+HasibulKabir@users.noreply.github.com>
2020-10-28 09:49:48 -0700

    fix for heroku...

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify
 from goodreads import GoodReads
 
@@ -6,15 +7,15 @@ api = Flask(__name__)
 @api.route('/')
 def index():
     res = {
-      'status': 200,
-      'about': 'Quotes api by W4RR10R',
-      'usage': [
-         '/random',
-         '/search/query/no of pages'
-       ]
+        'status': 200,
+        'about': 'Quotes api by W4RR10R',
+        'usage': [
+            '/random',
+            '/search/query/no of pages'
+        ]
     }
     return jsonify(res)
- 
+
 @api.route('/random')
 def random_quotes():
     return jsonify(GoodReads().random)
@@ -22,18 +23,18 @@ def random_quotes():
 @api.route('/search')
 def search():
     res = {
-       "error": {
-          'status': 400,
-          'detail': 'No search query found'
-       }
+        "error": {
+            'status': 400,
+            'detail': 'No search query found'
+        }
     }
     return jsonify(res)
- 
+
 @api.route('/search/<query>', defaults= {"pages": 1})  
 @api.route('/search/<query>/<pages>')
 def search_quotes(query, pages):
     results = GoodReads.search_all(query, int(pages))
     return jsonify(results)
-       
+
 if __name__ == '__main__':
-    api.run(host="0.0.0.0", port=5000, debug=True)
+    api.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
